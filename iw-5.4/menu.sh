0ff030b1f5dff3cf0c51b81bd395a356b1d9bc39
weihong <fc4305@faureciacoagent.com>
2020-05-09 03:55:50 -0400

    add iw-5.4

---
+++ iw-5.4/menu.sh
@@ -0,0 +1,11 @@
+ROOT=$1
+arm_gcc=${ROOT}/toolchain/gcc-linaro-7.2.1-2017.11-x86_64_arm-linux-gnueabihf/bin/arm-linux-gnueabihf-gcc
+rootfs_path=${ROOT}/mini_root/rootfs
+PKG_PATH=${rootfs_path}/lib/pkgconfig
+ext_libs=${rootfs_path}/lib 
+MYDIR=`dirname $0`
+cd ${MYDIR}
+export CC=${arm_gcc} PREFIX=${rootfs_path} PKG_CONFIG_PATH=${PKG_PATH}  MY_BASE=${ROOT}  EXT_LIBS=${ext_libs}
+make
+make install
+cd -
